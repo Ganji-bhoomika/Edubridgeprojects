51682fd10d40042427c35275a4c231e4168f0237
Ganji-bhoomika <bhoomikaganji2002@gmail.com>
2024-05-22 16:24:25 +0530

    Sample Commit

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,12 +3,19 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    
   ],
   bootstrap: [AppComponent],
+  providers: [
+    provideHttpClient(withFetch())
+  ]
+  
 })
 export class AppServerModule {}
